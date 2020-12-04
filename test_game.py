0db65494028c26ee5d86e27a926b248ba0343ef6
Rob Graziano <rgraziano.gzr7702@gmail.com>
2020-12-04 18:11:52 -0500

    update test

--- test_game.py
+++ test_game.py
@@ -1,15 +1,21 @@
+'''
+    Tests for card game. One integrated test for the game setup, and one for the game play.
+'''
+
 import unittest
 
 class TestGameSetup(unittest.TestCase):
+    ''' Test the game setup including the deck '''
     def setUp(self):
         ''' Create a card deck and two players '''
         self.deck = []
 
     def test_deck_proper_length(self):
+        ''' Is the deck the proper length? '''
         self.assertTrue(len(self.deck) == 52)
 
 class TestGame(unittest.TestCase):
-    pass
+    ''' Test that the game can be played properly '''
 
 if __name__ == "__main__":
     unittest.main()
