7100bc7b5671fec481e2635cf4a592962a33c3a1
gzr7702 <rgraziano.gzr7702@gmail.com>
2020-12-08 13:28:55 -0500

    basic game tests passed

---
+++ game.py
@@ -0,0 +1,44 @@
+""" The main module that runs the game.
+
+The game is played between two people taking turns at the same terminal.
+
+Typical usage example:
+
+python game.py
+
+"""
+
+import sys
+
+
+def get_command():
+    """ Wrapper for input() """
+
+    return input("Hit any key to continue")
+
+
+def main():
+    """Starts the card game.
+
+    Args: None
+
+    Returns: None
+
+    """
+
+    instructions = (
+        "Welecome! Hit any key for the next player " + "to take a turn, 'q' to quit."
+    )
+    print(instructions)
+
+    command = ""
+    for i in range(6):
+        command = get_command()
+        print(command)
+        if command == "q":
+            print("Ending play...")
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
--- test_game.py
+++ test_game.py
@@ -1,11 +1,14 @@
-"""
-    Tests for card game. One integrated test for the game setup, and one for the game play.
+""" These are the tests for card game. One integrated test for the game
+    setup, and one for the game play.
 """
 
 import unittest
+from unittest.mock import patch
+from io import StringIO
 
 from card import Card
 from deck import Deck
+import game
 
 
 class TestGameSetup(unittest.TestCase):
@@ -13,26 +16,31 @@ class TestGameSetup(unittest.TestCase):
 
     def setUp(self):
         """ Create a card deck and two players """
+
         self.sample_card = Card("clubs", "3")
         self.deck = Deck()
 
     def test_card_created_with_properties(self):
         """ Test card is created properly """
+
         self.assertTrue(
             (self.sample_card.suit == "clubs") and (self.sample_card.value == "3")
         )
 
     def test_card_str_method(self):
         """ Test card.__str__ exists and has the proper text """
+
         card_str = "3 of Clubs"
         self.assertTrue(str(self.sample_card), card_str)
 
     def test_deck_is_proper_length(self):
         """ Test deck length """
+
         self.assertTrue(self.deck.count == 52)
 
     def test_deck_has_all_suits(self):
-        """ Check deck suits """
+        """ Check deck all suits with 13 cards each """
+
         suit_count = {"Clubs": 0, "Hearts": 0, "Diamonds": 0, "Spades": 0}
 
         # count cards in all suits
@@ -44,8 +52,9 @@ class TestGameSetup(unittest.TestCase):
             self.assertEqual(card_count, 13)
 
     def test_deck_has_been_shuffled(self):
-        """Create a list of indeces for the ordered deck, then do the
-        same for the shuffled deck and assert that they are different"""
+        """Compare string of unshuffled and shuffled deck. We aren't
+        checking if the deck is in random order. We trust that the
+        the shuffle method takes care of that"""
 
         unshuffled_deck = repr(self.deck)
         self.deck.shuffle()
@@ -55,7 +64,25 @@ class TestGameSetup(unittest.TestCase):
 
 
 class TestGame(unittest.TestCase):
-    """ Test that the game can be played properly """
+    """Test that the game can be played properly"""
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_game_text(self, mock_stdout):
+        """ Test that the game text is correct. """
+        intro = (
+            "Welecome! Hit any key for the next "
+            + "player to take a turn, 'q' to quit.\n"
+        )
+        instructions = 6 * "Hit any key to continue\n"
+        text = intro + instructions
+        game.main()
+        self.assertEqual(mock_stdout.getvalue(), text)
+
+    @patch("builtins.input", return_value="q")
+    def test_player_input(self, input):
+        """ Test that the player input works. """
+        command = game.get_command()
+        self.assertEqual(command, "q")
 
 
 if __name__ == "__main__":
